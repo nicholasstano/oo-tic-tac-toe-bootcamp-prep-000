c206537ded9223efafd5c4b9e631f4774c26ceb4
Nicholas Stano <nicholasstano@gmail.com>
2019-02-23 16:34:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,7 +14,7 @@ WIN_COMBINATIONS = [
   [0, 4, 8],
   [6, 4, 2]]
 
-def display_board(board)
+def display_board
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "---------"
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
