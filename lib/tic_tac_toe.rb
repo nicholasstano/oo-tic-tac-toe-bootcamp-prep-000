83d489beae938af73ffe04337dc9a90be7a7b781
Nicholas Stano <nicholasstano@gmail.com>
2019-02-23 17:45:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -100,20 +100,6 @@ def winner
   end
 end
 
-def play
 
-turn(board) until over?(board)
-if won?(board)
-  puts "Congratulations #{winner(board)}!"
-elsif draw?(board)
-  puts "Cat's Game!"
-
-  n = 1 
-  while n < 10 
-  turn(board)
-  n += 1 
-
-end
-end
 
 end
