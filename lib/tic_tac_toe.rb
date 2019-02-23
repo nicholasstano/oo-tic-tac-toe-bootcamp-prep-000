4b024aaab29f36a650bdc814d3aaaaefef109040
Nicholas Stano <nicholasstano@gmail.com>
2019-02-23 17:04:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,4 +47,17 @@ else
 end
 end
 
+def turn
+
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(index)
+  move(@board, index, current_player(board))
+  display_board
+  else 
+  turn
+end
+end
+
 end
